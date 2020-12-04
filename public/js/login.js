6e04ebe510cb05d74eea0e66e4beb838af649895
François Poguet <francoispoguet@gmail.com>
2020-12-04 03:08:04 +0100

    correction login & signup

---
+++ public/js/login.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const inputEmail = document.getElementById("inputEmail");
+  const inputPassword = document.getElementById("inputPassword");
+  const btnSubmit = document.getElementById("btnSubmit");
+
+  btnSubmit.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const body = JSON.stringify({
+      userEmail: inputEmail.value.trim(),
+      userPassword: inputPassword.value.trim(),
+    });
+
+    const headers = {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    };
+
+    fetch("/user/login", {
+      method: "POST",
+      mode: "cors",
+      headers,
+      body,
+    })
+      .then((res) => {
+        res.text().then((txt) => {
+          localStorage.setItem("token", JSON.parse(txt).token);
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        // Afficher l'erreur
+      });
+  });
+});
