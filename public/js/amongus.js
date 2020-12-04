5049ced5c6a0fa91128b50e4d814615fc0f9cf95
Cynthia M <c.maid9425@gmail.com>
2020-12-04 04:54:59 +0100

    perso ok sauf stat + button emergency

--- public/js/amongus.js
+++ public/js/amongus.js
@@ -1,4 +1,5 @@
 var nbDechet = 4;
+var animauxToSave = 3;
 
 function playCollect(){
 	document.getElementById("screenCollect").style.display = 'block';
@@ -34,9 +35,47 @@ function collect(numb) {
 	}
 
 	if(nbDechet == 0){
-		quitCollect();
 		document.getElementById("bouteilleClear").style.display = 'block';
 		document.getElementById("bouteilleAlert").style.display = 'none';
-		//TODO
+		setTimeout(function(){ quitCollect(); }, 500);
+	}
+}
+
+
+function playPoulpe(){
+	document.getElementById("screenOcean").style.display = 'block';
+	document.getElementById("collectBouteille").style.display = 'block';
+	document.getElementById("collectPneu").style.display = 'block';
+	document.getElementById("collectGobelet").style.display = 'block';
+	document.getElementById("collectCanette").style.display = 'block';
+	animauxToSave = 3;
+}
+
+function quitPoulpe(){
+	document.getElementById("screenOcean").style.display = 'none';
+}
+
+function saveAnimaux(numb){
+	switch(numb) {
+		case 1 :
+			document.getElementById("poisson").style.display = 'block';
+			document.getElementById("poissonHelp").style.display = 'none';
+			animauxToSave--;
+		break;
+		case 2 :
+			document.getElementById("poulpeJeu").style.display = 'block';
+			document.getElementById("poulpeHelp").style.display = 'none';
+			animauxToSave--;
+		break;
+		case 3 :
+			document.getElementById("tortue").style.display = 'block';
+			document.getElementById("tortueHelp").style.display = 'none';
+			animauxToSave--;
+		break;
+	}
+	if(animauxToSave==0){
+		document.getElementById("poulpe").style.display = 'block';
+		document.getElementById("poulpeAlert").style.display = 'none';
+		setTimeout(function(){ quitPoulpe(); }, 500);
 	}
 }
